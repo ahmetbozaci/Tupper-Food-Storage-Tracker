7cf4062937cbb93e35ce11e3afe1205eade65c34
omodauda <omodauda.dl@gmail.com>
2022-10-05 16:38:40 +0100

    Custom button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,41 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
+import {heightPercentage, fontSz} from '../config';
+import COLORS from '../color';
 
-const Button: React.FC = () => {
+interface ButtonProps {
+  buttonText: string;
+  buttonStyle?: object;
+  buttonTextStyle?: object;
+  onPress?: Function;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  buttonText,
+  buttonStyle,
+  buttonTextStyle,
+}) => {
   return (
-    <View>
-      <Text>Button: React.FC</Text>
-    </View>
+    <TouchableOpacity style={[styles.button, buttonStyle]}>
+      <Text style={[styles.text, buttonTextStyle]}>{buttonText}</Text>
+    </TouchableOpacity>
   );
 };
 
 export default Button;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 16,
+    paddingVertical: heightPercentage(8),
+    backgroundColor: COLORS.green,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: fontSz(14),
+    textAlign: 'center',
+  },
+});
