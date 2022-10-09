36f5fa0079a4f9d0bbcced1ed262332eddd13519
Ahmet Bozacı <ahmt9417@gmail.com>
2022-10-10 00:56:08 +0300

    Add login form

--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -1,13 +1,74 @@
-import {SafeAreaView, Text} from 'react-native';
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {useState} from 'react';
+import {
+  Button,
+  TextInput,
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+import {Formik} from 'formik';
 import styles from './styles';
+import {initialValuesInterface} from './types';
+import validationSchema from './validationSchema';
+import CustomButton from '../../shared/Button';
 
-const Login: React.FC = () => {
+const UserForm: React.FC = () => {
+  const [userInformation, setUserInformation] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+  });
+  const initialValues: initialValuesInterface = {
+    email: '',
+    password: '',
+  };
   return (
-    <SafeAreaView style={styles.screen}>
-      <Text>Login</Text>
-    </SafeAreaView>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values, actions) => {
+        actions.resetForm();
+        setUserInformation(values);
+      }}
+    >
+      {({values, handleChange, handleSubmit, isValid, errors, touched}) => (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.screen}>
+            <Text style={styles.titleText}>Log In</Text> 
+            <TextInput
+              style={styles.input}
+              multiline
+              placeholder="Email"
+              onChangeText={handleChange('email')}
+              value={values.email}
+            />
+            {touched.email && errors.email && (
+              <Text style={styles.errorText}>{errors.email}</Text>
+            )}
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              onChangeText={handleChange('password')}
+              value={values.password}
+            />
+            {touched.password && errors.password && (
+              <Text style={styles.errorText}>{errors.password}</Text>
+            )}
+
+            <CustomButton
+              buttonText="Log In"
+              onPress={handleSubmit}
+              disabled={!isValid}
+              // onPress={() => navigation.navigate('Signup')}
+            />
+          </View>
+        </TouchableWithoutFeedback>
+      )}
+    </Formik>
   );
 };
 
-export default Login;
+export default UserForm;
--- src/Screens/Login/styles.tsx
+++ src/Screens/Login/styles.tsx
@@ -1,8 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {StyleSheet} from 'react-native';
+import COLORS from '../../color';
+import {fontSz, widthPercentage, heightPercentage} from '../../config';
 
 const styles = StyleSheet.create({
+  titleText: {
+    fontSize: fontSz(40),
+    fontWeight: 'bold',
+    color: COLORS.black3,
+    textAlign: 'center',
+    paddingBottom: 30,
+  },
+  errorText: {
+    fontSize: 12,
+    color: '#FF0D10',
+  },
   screen: {
     flex: 1,
+    paddingHorizontal: widthPercentage(30),
+    justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: COLORS.gray4,
+    paddingVertical: heightPercentage(15),
+    paddingHorizontal: widthPercentage(30),
+    marginVertical: heightPercentage(5),
+    fontSize: fontSz(20),
+    borderRadius: 50,
   },
 });
 
