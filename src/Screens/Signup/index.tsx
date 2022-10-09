9adb9e9f169d22331c10f5af7dc9bebc9cc28a7a
Ahmet Bozacı <ahmt9417@gmail.com>
2022-10-09 18:13:25 +0300

    Update signup form

--- src/Screens/Signup/index.tsx
+++ src/Screens/Signup/index.tsx
@@ -1,15 +1,105 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import {Text, SafeAreaView} from 'react-native';
-import React from 'react';
-import styles from '../Main/styles';
-import UserForm from '../../components/UserForm';
+import React, {useState} from 'react';
+import {Button, TextInput, View, Text} from 'react-native';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
+import styles from './styles';
 
-const Signup: React.FC = () => {
+const UserForm = () => {
+  const [userInformation, setUserInformation] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+  });
+
   return (
-    <SafeAreaView style={styles.screen}>
-      <UserForm />
-    </SafeAreaView>
+    <View>
+      <Formik
+        initialValues={{
+          name: '',
+          email: '',
+          password: '',
+          passwordConfirmation: '',
+          agree: false,
+        }}
+        //! object().shape() or only object() ????
+        validationSchema={Yup.object({
+          name: Yup.string().required('Please, provide your name'),
+          email: Yup.string().email().required('Please provide your email'),
+          password: Yup.string()
+            .min(8, 'Password should minimum 8 characters.')
+            .required(),
+          passwordConfirmation: Yup.string().when('password', {
+            is: (val: string | any[]) => (val && val.length > 0 ? true : false),
+            then: Yup.string().oneOf(
+              [Yup.ref('password')],
+              'Both password need to be the same',
+            ),
+          }),
+        })}
+        onSubmit={values => {
+          setUserInformation(values);
+          console.log('hello');
+        }}>
+        {({values, handleChange, handleSubmit, dirty, errors, touched}) => (
+          <View>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter username"
+              onChangeText={handleChange('name')}
+              value={values.name}
+            />
+            {touched.name && errors.name && (
+              <Text style={{fontSize: 12, color: '#FF0D10'}}>
+                {errors.name}
+              </Text>
+            )}
+            <TextInput
+              style={styles.input}
+              multiline
+              placeholder="Enter email"
+              onChangeText={handleChange('email')}
+              value={values.email}
+            />
+            {touched.email && errors.email && (
+              <Text style={{fontSize: 12, color: '#FF0D10'}}>
+                {errors.email}
+              </Text>
+            )}
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              onChangeText={handleChange('password')}
+              value={values.password}
+            />
+            {touched.password && errors.password && (
+              <Text style={{fontSize: 12, color: '#FF0D10'}}>
+                {errors.password}
+              </Text>
+            )}
+            <TextInput
+              style={styles.input}
+              placeholder="Password Confirmation"
+              onChangeText={handleChange('passwordConfirmation')}
+              value={values.passwordConfirmation}
+            />
+            {touched.passwordConfirmation && errors.passwordConfirmation && (
+              <Text style={{fontSize: 12, color: '#FF0D10'}}>
+                {errors.passwordConfirmation}
+              </Text>
+            )}
+            <Button
+              color="maroon"
+              title="Submit"
+              onPress={handleSubmit}
+              disabled={!dirty}
+            />
+          </View>
+        )}
+      </Formik>
+    </View>
   );
 };
 
-export default Signup;
+export default UserForm;
--- src/Screens/Signup/styles.tsx
+++ src/Screens/Signup/styles.tsx
@@ -1,8 +1,24 @@
 import {StyleSheet} from 'react-native';
-
 const styles = StyleSheet.create({
-  screen: {
+  titleText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+  },
+  container: {
     flex: 1,
+    padding: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 10,
+    fontSize: 18,
+    borderRadius: 6,
   },
 });
 
