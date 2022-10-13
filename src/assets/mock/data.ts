e73f9f9536a228bd3acf6edd20abd5b7aee09323
omodauda <omodauda.dl@gmail.com>
2022-10-13 21:31:02 +0100

    Mock data

---
+++ src/assets/mock/data.ts
@@ -0,0 +1,60 @@
+const DATA = {
+  storages: [
+    {
+      id: '1',
+      title: 'Fridge',
+      items: [
+        {
+          id: '1',
+          name: 'Pizza',
+          created_date: 'Yesterday',
+          expiry_date: '12/12/22',
+        },
+        {
+          id: '1',
+          name: 'Pizza',
+          created_date: 'Yesterday',
+          expiry_date: '12/12/22',
+        },
+      ],
+    },
+    {
+      id: '2',
+      title: 'Pantry',
+      items: [
+        {
+          id: '1',
+          name: 'Pizza',
+          created_date: 'Yesterday',
+          expiry_date: '12/12/22',
+        },
+        {
+          id: '1',
+          name: 'Pizza',
+          created_date: 'Yesterday',
+          expiry_date: '12/12/22',
+        },
+      ],
+    },
+    {
+      id: '3',
+      title: 'Freezer',
+      items: [
+        {
+          id: '1',
+          name: 'Pizza',
+          created_date: 'Yesterday',
+          expiry_date: '12/12/22',
+        },
+        {
+          id: '1',
+          name: 'Pizza',
+          created_date: 'Yesterday',
+          expiry_date: '12/12/22',
+        },
+      ],
+    },
+  ],
+};
+
+export default DATA;
