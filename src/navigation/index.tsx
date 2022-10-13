90494eb7360d657a329cd6e4d0bb427b3e4d62a0
omodauda <omodauda.dl@gmail.com>
2022-10-13 13:11:51 +0100

    Setup bottom tab navigation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import SignupScreen from '../Screens/Signup';
 import LoginScreen from '../Screens/Login';
 import MainScreen from '../Screens/Main';
 
+// Bottom Tab Screens
+import HomeScreen from '../Screens/Home';
+import AddScreen from '../Screens/Add';
+import StorageScreen from '../Screens/Storage';
+
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Add" component={AddScreen} />
+      <Tab.Screen name="Storage" component={StorageScreen} />
+    </Tab.Navigator>
+  );
+};
 
 const MainStack = () => {
   return (
@@ -15,9 +35,10 @@ const MainStack = () => {
         screenOptions={{
           headerShown: false,
         }}>
-        <Stack.Screen name="Main" component={MainScreen} />
+        {/* <Stack.Screen name="Main" component={MainScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} /> */}
+        <Stack.Screen name="Tabs" component={Tabs} />
       </Stack.Navigator>
     </NavigationContainer>
   );
