a9bc044b91b5f9d86c0d083d92c234093958585d
omodauda <omodauda.dl@gmail.com>
2022-10-13 15:46:28 +0100

    use custom tab bar component

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  createBottomTabNavigator,
+  BottomTabBarProps,
+} from '@react-navigation/bottom-tabs';
 
-import SignupScreen from '../Screens/Signup';
-import LoginScreen from '../Screens/Login';
-import MainScreen from '../Screens/Main';
+// import SignupScreen from '../Screens/Signup';
+// import LoginScreen from '../Screens/Login';
+// import MainScreen from '../Screens/Main';
 
 // Bottom Tab Screens
 import HomeScreen from '../Screens/Home';
 import AddScreen from '../Screens/Add';
 import StorageScreen from '../Screens/Storage';
+import COLORS from '../color';
+import BottomTabBar from './TabBarComponent';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -18,8 +23,11 @@ const Tab = createBottomTabNavigator();
 const Tabs = () => {
   return (
     <Tab.Navigator
+      tabBar={(props: BottomTabBarProps) => <BottomTabBar {...props} />}
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: COLORS.primary,
+        tabBarInactiveTintColor: COLORS.gray,
       }}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Add" component={AddScreen} />
