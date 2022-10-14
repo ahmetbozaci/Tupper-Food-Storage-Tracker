f00430dd46416aee6007c80b0483e8a23526553a
omodauda <omodauda.dl@gmail.com>
2022-10-14 21:05:56 +0100

    Freezer screen

---
+++ src/Screens/Foods/Freezer/index.tsx
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function Freezer() {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
