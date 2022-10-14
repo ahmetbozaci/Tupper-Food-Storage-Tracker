54be42992174724c171c67e2d30d7089846e8c73
omodauda <omodauda.dl@gmail.com>
2022-10-14 21:05:39 +0100

    Pantry screen

---
+++ src/Screens/Foods/Pantry/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Pantry() {
+  return (
+    <View>
+      <Text>Pantry</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
