6e8c53afe7e0ba2ef5a9d55d1def97937e1c7cb3
omodauda <omodauda.dl@gmail.com>
2022-10-20 16:22:46 +0100

    Reconfigure store

--- src/features/store.tsx
+++ src/features/store.tsx
@@ -1,18 +1,53 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import {useDispatch, useSelector, TypedUseSelectorHook} from 'react-redux';
-import forgotPasswordSlice from './forgotPasswordSlice';
-import loginSlice from './loginSlice';
-import resetPasswordSlice from './resetPasswordSlice';
-import signupSlice from './signupSlice';
+// import forgotPasswordSlice from './forgotPasswordSlice';
+// import loginSlice from './loginSlice';
+// import resetPasswordSlice from './resetPasswordSlice';
+// import signupSlice from './signupSlice';
+import authSlice from './authSlice';
+import {
+  persistReducer,
+  persistStore,
+  PERSIST,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const reducers = combineReducers({
+  auth: authSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: {
-    login: loginSlice,
-    signup: signupSlice,
-    forgotPassword: forgotPasswordSlice,
-    resetPassword: resetPasswordSlice,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+// const store = configureStore({
+//   reducer: {
+//     auth: authSlice,
+//     login: loginSlice,
+//     signup: signupSlice,
+//     forgotPassword: forgotPasswordSlice,
+//     resetPassword: resetPasswordSlice,
+//   },
+// });
+export const persistor = persistStore(store);
 export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
