1664d2664c712fb0f8c849d9d66901cd74b90b0c
omodauda <omodauda.dl@gmail.com>
2022-10-26 11:03:18 +0100

    Refactor foods screen

--- src/Screens/Foods/Freezer/index.tsx
+++ src/Screens/Foods/Freezer/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import FoodList from '../../../shared/FoodList';
-import {useQuery} from '@tanstack/react-query';
-import {fetchFoodsByStorage} from '../../../api/food';
+// import {useQuery} from '@tanstack/react-query';
+// import {fetchFoodsByStorage} from '../../../api/food';
 
 const Freezer: React.FC = () => {
   const title = 'Freezer';
-  const {data} = useQuery(['Freezer'], () => fetchFoodsByStorage(title), {
-    enabled: true,
-    retry: false,
-  });
+  // const {data} = useQuery(['Freezer'], () => fetchFoodsByStorage(title), {
+  //   enabled: true,
+  //   retry: false,
+  // });
 
-  return <FoodList headerTitle={title} data={data} />;
+  return <FoodList headerTitle={title} />;
 };
 
 export default Freezer;
--- src/Screens/Foods/Fridge/index.tsx
+++ src/Screens/Foods/Fridge/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import FoodList from '../../../shared/FoodList';
-import {useQuery} from '@tanstack/react-query';
-import {fetchFoodsByStorage} from '../../../api/food';
+// import {useQuery} from '@tanstack/react-query';
+// import {fetchFoodsByStorage} from '../../../api/food';
 
 const Fridge: React.FC = () => {
   const title = 'Fridge';
-  const {data} = useQuery(['Fridge'], () => fetchFoodsByStorage(title), {
-    enabled: true,
-    retry: false,
-  });
+  // const {data} = useQuery(['Fridge'], () => fetchFoodsByStorage(title), {
+  //   enabled: true,
+  //   retry: false,
+  // });
 
-  return <FoodList headerTitle={title} data={data} />;
+  return <FoodList headerTitle={title} />;
 };
 
 export default Fridge;
