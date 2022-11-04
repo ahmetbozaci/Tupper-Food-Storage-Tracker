a184f36caef407b65c087a39a69a6cb962a564d4
omodauda <omodauda.dl@gmail.com>
2022-11-04 10:19:51 +0100

    Add font family for onboarding screen texts

--- src/Screens/Onboarding/styles.tsx
+++ src/Screens/Onboarding/styles.tsx
@@ -4,16 +4,17 @@ import {fontSz, widthPercentage, heightPercentage} from '../../config';
 const styles = StyleSheet.create({
   subtitle: {
     color: COLORS.black,
-    fontSize: fontSz(22),
-    textAlign: 'center',
-    lineHeight: 24,
+    fontSize: fontSz(16),
+    fontFamily: 'Poppins-SemiBold',
     marginHorizontal: widthPercentage(50),
     marginBottom: heightPercentage(25),
-    fontWeight: 'bold',
+    fontWeight: '600',
+    maxWidth: widthPercentage(240),
   },
   title: {
     color: COLORS.green3,
-    fontWeight: 'bold',
+    fontWeight: '600',
+    fontFamily: 'Poppins-Bold',
     fontSize: fontSz(55),
     marginVertical: heightPercentage(20),
     marginHorizontal: widthPercentage(30),
@@ -39,6 +40,8 @@ const styles = StyleSheet.create({
   btnText: {
     fontWeight: '500',
     fontSize: fontSz(16),
+    fontFamily: 'Helvetica-Regular',
+    color: COLORS.white,
   },
   backArrowContainer: {
     marginTop: heightPercentage(50),
